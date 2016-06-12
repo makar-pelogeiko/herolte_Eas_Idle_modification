06e7906bd372b57e27d0607b59b79ecb7685ab1a
Olav Haugan <ohaugan@codeaurora.org>
2016-06-12 13:57:05 -0700

    BACKPORT: sched/core: Add trace point for cpu isolation
    Add tracepoint to capture the cpu isolation event including KPI for
    time it took to isolate.
    
    Change-Id: If2d30000f068afc50db953940f4636ef6a089b24
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5467,6 +5467,10 @@ int sched_isolate_cpu(int cpu)
 	struct rq *rq = cpu_rq(cpu);
 	cpumask_t avail_cpus;
 	int ret_code = 0;
+	u64 start_time;
+
+	if (trace_sched_isolate_enabled())
+		start_time = sched_clock();
 
 	lock_device_hotplug();
 
@@ -5502,6 +5506,8 @@ int sched_isolate_cpu(int cpu)
 
 out:
 	unlock_device_hotplug();
+	trace_sched_isolate(cpu, cpumask_bits(cpu_isolated_mask)[0],
+			    start_time, 1);
 	return ret_code;
 }
 
@@ -5515,6 +5521,10 @@ int sched_unisolate_cpu_unlocked(int cpu)
 {
 	int ret_code = 0;
 	struct rq *rq = cpu_rq(cpu);
+	u64 start_time;
+
+	if (trace_sched_isolate_enabled())
+		start_time = sched_clock();
 
 	lock_device_hotplug_assert();
 
@@ -5551,6 +5561,8 @@ int sched_unisolate_cpu_unlocked(int cpu)
 	}
 
 out:
+	trace_sched_isolate(cpu, cpumask_bits(cpu_isolated_mask)[0],
+			    start_time, 0);
 	return ret_code;
 }
 

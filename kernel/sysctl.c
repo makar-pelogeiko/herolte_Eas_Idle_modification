d5caf89553c7f1e85f2662b2e9721dde9f6479a2
Patrick Bellasi <patrick.bellasi@arm.com>
2016-10-12 19:19:28 +0100

    FIXUP: kernel/sysctl: allows negative values for global boosting
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Signed-off-by: Srinath Sridharan <srinathsr@google.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -133,6 +133,7 @@ static int one_hundred = 100;
 #ifdef CONFIG_INCREASE_MAXIMUM_SWAPPINESS
 static int max_swappiness = 200;
 #endif
+static int __maybe_unused one_hundred_neg = -100;
 #ifdef CONFIG_PRINTK
 static int ten_thousand = 10000;
 #endif
@@ -520,7 +521,7 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 #endif
 		.proc_handler	= &sysctl_sched_cfs_boost_handler,
-		.extra1		= &zero,
+		.extra1		= &one_hundred_neg,
 		.extra2		= &one_hundred,
 	},
 #endif

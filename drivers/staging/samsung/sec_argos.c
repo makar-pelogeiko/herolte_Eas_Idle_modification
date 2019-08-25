d553ac7d22350f9ae768a3ea9af0b12afbdc363b
Diep Quynh <remilia.1505@gmail.com>
2019-08-26 03:09:58 +0700

    argos: Adapt global boost
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- drivers/staging/samsung/sec_argos.c
+++ drivers/staging/samsung/sec_argos.c
@@ -28,6 +28,13 @@
 #include <linux/sec_argos.h>
 #include <linux/proc_fs.h>
 
+#if defined(CONFIG_SCHED_EMS)
+#include <linux/ems.h>
+static struct gb_qos_request gb_req = {
+	.name = "argos_global_boost",
+};
+#endif
+
 #define ARGOS_NAME "argos"
 #define TYPE_SHIFT 4
 #define TYPE_MASK_BIT ((1 << TYPE_SHIFT) - 1)
@@ -377,12 +384,20 @@ int argos_hmpboost_apply(int dev_num, bool enable)
 	if (enable) {
 		/* disable -> enable */
 		if (*hmpboost_enable == false) {
+#if defined(CONFIG_SCHED_EMS)
+			/* set global boost */
+			gb_qos_update_request(&gb_req, 100);
+#endif
 			*hmpboost_enable = true;
 			pr_info("%s: hmp boost enable [%d]\n", __func__, dev_num);
 		}
 	} else {
 		/* enable -> disable */
 		if (*hmpboost_enable == true) {
+#if defined(CONFIG_SCHED_EMS)
+			/* unset global boost */
+			gb_qos_update_request(&gb_req, 0);
+#endif
 			*hmpboost_enable = false;
 			pr_info("%s: hmp boost disable [%d]\n", __func__, dev_num);
 		}

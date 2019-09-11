77ba7abd00fabe2cba445ef934a30051428e5d82
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-09-11 19:51:36 +0800

    exynos hotplug: reduce loadavg

--- drivers/soc/samsung/exynos-hotplug_governor.c
+++ drivers/soc/samsung/exynos-hotplug_governor.c
@@ -253,7 +253,7 @@ static int exynos_hpgov_do_update_governor(void *data)
 	int req_cpu_min;
 
 	while (1) {
-		wait_event(exynos_hpgov.wait_q, pdata->event || kthread_should_stop());
+		wait_event_interruptible(exynos_hpgov.wait_q, pdata->event || kthread_should_stop());
 		if (kthread_should_stop())
 			break;
 
@@ -283,7 +283,7 @@ static int exynos_hpgov_do_hotplug(void *data)
 	int last_min = 0;
 
 	while (1) {
-		wait_event(exynos_hpgov.wait_hpq, *event || kthread_should_stop());
+		wait_event_interruptible(exynos_hpgov.wait_hpq, *event || kthread_should_stop());
 		if (kthread_should_stop())
 			break;
 

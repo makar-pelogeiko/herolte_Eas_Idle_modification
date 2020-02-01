a08001a4b82d3810dea75258ce5596f840102785
Samuel Pascua <pascua.samuel.14@gmail.com>
2020-02-01 11:04:23 +0800

    random: always use /dev/urandom

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -1372,12 +1372,6 @@ _random_read(int nonblock, char __user *buf, size_t nbytes)
 }
 
 static ssize_t
-random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
-{
-	return _random_read(file->f_flags & O_NONBLOCK, buf, nbytes);
-}
-
-static ssize_t
 urandom_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 {
 	int ret;
@@ -1505,7 +1499,7 @@ static int random_fasync(int fd, struct file *filp, int on)
 }
 
 const struct file_operations random_fops = {
-	.read  = random_read,
+	.read  = urandom_read,
 	.write = random_write,
 	.poll  = random_poll,
 	.unlocked_ioctl = random_ioctl,

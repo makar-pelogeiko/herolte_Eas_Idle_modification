67d0f4ab362d2dc2a4e7212f27cf579b4f7db68d
Samuel Pascua <pascua.samuel.14@gmail.com>
2020-05-31 18:06:15 +0800

    Revert "sched: core: fix bug that rt could not be binded to little"
    This reverts commit feea1a9a95df378b148c26bda26ec63d4b1faa58.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4052,15 +4052,6 @@ change:
 		enqueue_task(rq, p, queue_flags);
 	}
 
-	if (p->sched_class == &rt_sched_class) {
-		struct cpumask mask;
-
-		cpumask_andnot(&mask, &p->cpus_allowed, cpu_coregroup_mask(4));
-		cpumask_andnot(&mask, &p->cpus_allowed, cpu_coregroup_mask(6));
-		if (!cpumask_empty(&mask))
-			do_set_cpus_allowed(p, &mask);
-	}
-
 	check_class_changed(rq, p, prev_class, oldprio);
 	task_rq_unlock(rq, p, &flags);
 

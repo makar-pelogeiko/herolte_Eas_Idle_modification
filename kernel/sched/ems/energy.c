4897ae17f2ada94bb3ffcaa58aa55b0f2e816026
Samuel Pascua <pascua.samuel.14@gmail.com>
2020-05-31 19:48:54 +0800

    ems: don't inline extern function

--- kernel/sched/ems/energy.c
+++ kernel/sched/ems/energy.c
@@ -38,7 +38,7 @@ struct energy_table {
 };
 DEFINE_PER_CPU(struct energy_table, energy_table);
 
-inline unsigned int get_cpu_mips(unsigned int cpu)
+unsigned int get_cpu_mips(unsigned int cpu)
 {
 	return per_cpu(energy_table, cpu).mips;
 }

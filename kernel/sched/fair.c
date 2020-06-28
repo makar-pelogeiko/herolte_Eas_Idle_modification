1c29a2d44cfdbf0cfb05406b1001848277c43a3e
Samuel Pascua <sgpascua@ngcp.ph>
2020-06-28 12:20:13 +0800

    Revert "Experimental!: sched/fair: Do not force want_affine eq. true if EAS is enabled"
    This reverts commit fd49daae1181c567eae69fa3aedc4ddbf2b10ba8.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5801,8 +5801,9 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 		return prev_cpu;
 
 	if (sd_flag & SD_BALANCE_WAKE)
-		want_affine = !wake_wide(p) && !wake_cap(p, cpu, prev_cpu)
-			      && cpumask_test_cpu(cpu, tsk_cpus_allowed(p));
+		want_affine = (!wake_wide(p) && !wake_cap(p, cpu, prev_cpu)
+			       && cpumask_test_cpu(cpu, tsk_cpus_allowed(p)))
+			      || energy_aware();
 
 	rcu_read_lock();
 	for_each_domain(cpu, tmp) {

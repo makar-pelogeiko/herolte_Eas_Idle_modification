c3bba25e75ee9697eb881bba64d022b043b8cecf
Samuel Pascua <sgpascua@ngcp.ph>
2020-07-13 08:01:05 +0800

    build: fix compiler string

--- build.sh
+++ build.sh
@@ -107,13 +107,12 @@ CONFIG_HALL_EVENT_REVERSE=y
 
 	#FUNC_CLEAN_DTB
 
-	export KBUILD_COMPILER_STRING=$($CLANG_DIR --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-
 	make -j$BUILD_JOB_NUMBER ARCH=$ARCH \
 			tmp_defconfig || exit -1
 
 	if [ $CC_NAME == "clang" ]; then
 		make -j$BUILD_JOB_NUMBER ARCH=$ARCH \
+			KBUILD_COMPILER_STRING=$($CLANG_DIR --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//') \
 			CC=$CLANG_DIR \
 			CLANG_TRIPLE=aarch64-linux-gnu- \
 			CROSS_COMPILE=$GCC_DIR || exit -1

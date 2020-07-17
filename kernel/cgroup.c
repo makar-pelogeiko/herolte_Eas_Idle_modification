26a3c4d0722a3e33cb0f0926ea7cff9aacbaff8b
Samuel Pascua <sgpascua@ngcp.ph>
2020-07-17 08:09:20 +0800

    Revert "cgroup: release css->id after css_free"
    This reverts commit fe9d97ad4b8e6be59470f192a657dc2c90ce3530.

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -4374,20 +4374,16 @@ static void css_free_work_fn(struct work_struct *work)
 {
 	struct cgroup_subsys_state *css =
 		container_of(work, struct cgroup_subsys_state, destroy_work);
-	struct cgroup_subsys *ss = css->ss;
 	struct cgroup *cgrp = css->cgroup;
 
 	percpu_ref_exit(&css->refcnt);
 
-	if (ss) {
+	if (css->ss) {
 		/* css free path */
-		int id = css->id;
-
 		if (css->parent)
 			css_put(css->parent);
 
-		ss->css_free(css);
-		cgroup_idr_remove(&ss->css_idr, id);
+		css->ss->css_free(css);
 		cgroup_put(cgrp);
 	} else {
 		/* cgroup free path */
@@ -4439,7 +4435,7 @@ static void css_release_work_fn(struct work_struct *work)
 
 	if (ss) {
 		/* css release path */
-		cgroup_idr_replace(&ss->css_idr, NULL, css->id);
+		cgroup_idr_remove(&ss->css_idr, css->id);
 		if (ss->css_released)
 			ss->css_released(css);
 	} else {

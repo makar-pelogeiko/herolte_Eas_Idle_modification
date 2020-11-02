1eb9c1fe434b1e784d786dff234b6956a72786dc
Samuel Pascua <sgpascua@ngcp.ph>
2020-11-02 16:48:56 +0800

    build: script: remove tweaker apps
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

---
+++ build/zip/common/script/remove_tweaks.sh
@@ -0,0 +1,15 @@
+#!/sbin/sh
+
+rm -rf /data/app/com.moro.mtweaks*
+rm -rf /data/data/com.moro.mtweaks
+rm -rf /data/.mtweaks
+rm -rf /data/.morokernel
+
+
+rm -rf /data/app/com.thunder.thundertweaks*
+rm -rf /data/data/com.thunder.thundertweaks
+rm -rf /data/.thundertweaks
+rm -rf /data/.tskernel
+
+rm -rf /data/data/com.hades.hKtweaks
+rm -rf /data/.hKtweaks
